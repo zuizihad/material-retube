b49e577b9f9f096bf730329441c70761351636fd
zuizihad <ahmedxihad@gmail.com>
2020-05-13 04:10:12 +0600

    working with search and fetch api data

--- src/App.js
+++ src/App.js
@@ -1,17 +1,31 @@
 import React from 'react'
 import  { Grid } from '@material-ui/core'
-import {SearchBar, VideoDetails, VideoList } from './components'
+import {SearchBar, VideoDetails } from './components'
 
 import youtube from './api/youtube'
 
 class App extends React.Component {
+
+    handleSubmit = async (searchTerm) => {
+        const response = await youtube.get('search', {
+            params: {
+                part: 'snipet',
+                maxResults: 5,
+                key: 'AIzaSyBJ0MfIC5Od8I6qtEZ2DGhmaWjnvSfxtYo',
+                q: searchTerm
+            }
+            })
+            console.log(response.data.items);
+            
+
+    }
     render() {
         return(
-            <Grid justify="center" container spacing={16}>
+            <Grid justify="center" container spacing={10}>
                 <Grid item xs={12}>
-                    <Grid container spacing={16}>
+                    <Grid container spacing={10}>
                         <Grid item xs={12}>
-                            <SearchBar/>
+                            <SearchBar onFormSubmit={this.handleSubmit}/>
                         </Grid>
                         <Grid item xs={8}> 
                             <VideoDetails/>
--- src/api/youtube.js
+++ src/api/youtube.js
@@ -2,9 +2,5 @@ import axios from 'axios'
 
 export default axios.create ({
     baseURL: 'http://www.google.com/youtbe/v3',
-    params: {
-        part: 'snipet',
-        maxResults: 5,
-        key: '[apikey]'
-    }
+  
 })
